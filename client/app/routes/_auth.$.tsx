1c138c374a46c4a72e1ca8e7d7eb610cdd88c066
Djibril <najidjibril@gmail.com>
2025-03-07 16:00:25 +0100

    chore : run prettier

--- client/app/routes/_auth.$.tsx
+++ client/app/routes/_auth.$.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 export default function NotFound() {
   return (
     <div className="m-auto flex h-full flex-col items-center justify-center gap-10">
-      <h1 className="text-brown text-7xl font-bold">404</h1>
+      <h1 className="text-7xl font-bold text-brown">404</h1>
       <p className="font-uniSansItalic text-2xl font-semibold">Oops... page non trouvé</p>
       <img src={routes.img.notFound} alt="Remix" className="h-96 object-contain" />
     </div>
